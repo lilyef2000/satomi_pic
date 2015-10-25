1029cca6067f9f524d19a5d5dfc0f3b88c5c30a9
lilyef2000 <lilyef2000@gmail.com>
2015-10-25 20:04:19 +0800

    add code in items.py

--- satomi_pic/items.py
+++ satomi_pic/items.py
@@ -11,4 +11,6 @@ import scrapy
 class SatomiPicItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    # pass
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
